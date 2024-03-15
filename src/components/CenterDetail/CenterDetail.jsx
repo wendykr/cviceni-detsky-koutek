2847c1c3b7be87360b4cccf67168a1d8ed588fdd
Vendula Krajíčková <vendysacek@seznam.cz>
2024-03-15 09:52:40 +0100

    add object.entries centerData.open

--- src/components/CenterDetail/CenterDetail.jsx
+++ src/components/CenterDetail/CenterDetail.jsx
@@ -4,18 +4,27 @@ import { useParams } from 'react-router-dom';
 export const CenterDetail = () => {
   const [centers, setCenters] = useState([]);
   const { centerId } = useParams();
-  const centerData = centers.find((center) => center.id == centerId);
+  const centerData = centers.find((center) => center.id == Number(centerId));
 
   useEffect(() => {
     const fetchCenters = async () => {
       const response = await fetch('http://localhost:4000/api/centers');
       const data = await response.json();
-      console.log(data.result);
       setCenters(data.result);
     };
 
     fetchCenters();
   }, []);
+
+  const dayTranslations = {
+    mon: 'Pondělí',
+    tue: 'Úterý',
+    wed: 'Středa',
+    thu: 'Čtvrtek',
+    fri: 'Pátek',
+    sat: 'Sobota',
+    sun: 'Neděle'
+  };
 
   return (
     <div className="container">
@@ -23,20 +32,20 @@ export const CenterDetail = () => {
         <>
           <h3>Detail pobočky - {centerData.name}</h3>
           <h4>{centerData.address}</h4>
-          {/* {centerData && centerData.open ? (
+          {centerData && !centerData.open ? (
             <>
               <p>Otevírací doba:</p>
               <div>
-                {centerData.open.map(oneDay => (
-                  <div key={oneDay.day}>
-                    <p>{oneDay.day}: {oneDay.open} - {oneDay.close}</p>
+                {Object.entries(centerData.open).map(([day, time], index) => (
+                  <div key={index}>
+                    <p>{dayTranslations[day]}: {time || 'Zavřeno'}</p>
                   </div>
                 ))}
               </div>
             </>
           ) : (
             <p><strong>Informace o otevírací době není k dispozici.</strong></p>
-          )} */}
+          )}
           <p>{centerData.info}</p>
         </>
       ) : (
