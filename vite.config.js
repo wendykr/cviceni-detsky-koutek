5a8c8ed37544dee7e507a0bb0757e55db49c2253
Vendula Krajíčková <vendysacek@seznam.cz>
2024-03-16 19:07:33 +0100

    set gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/cviceni-detsky-koutek/',
   root: './src',
-  publicDir: '../public',
+  publicDir: '../../public',
   server: {
     hmr: true,
   },
